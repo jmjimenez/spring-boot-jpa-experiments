7e1c732e50617846fec958fce23537071492ffe1
Jose Manuel Jimenez <jose.jimenezgarcia@enreach.com>
2025-08-21 16:44:14 +0200

    Some small refactors

--- src/main/java/es/jmjg/experiments/ExperimentsApplication.java
+++ src/main/java/es/jmjg/experiments/ExperimentsApplication.java
@@ -2,10 +2,8 @@ package es.jmjg.experiments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class ExperimentsApplication {
 
   public static void main(String[] args) {
--- src/main/java/es/jmjg/experiments/infrastructure/config/AppProperties.java
+++ src/main/java/es/jmjg/experiments/infrastructure/config/AppProperties.java
@@ -3,11 +3,13 @@ package es.jmjg.experiments.infrastructure.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Component
+@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "app")
 @Validated
 @Data
