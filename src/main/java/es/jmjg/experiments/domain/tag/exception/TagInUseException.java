50c7d0cc9c716b2991cc34f87acfe1173fe5e592
Jose Manuel Jimenez <jmjimenezg@gmail.com>
2025-09-20 10:30:45 +0200

    Remove TODO already done

--- src/main/java/es/jmjg/experiments/domain/tag/exception/TagInUseException.java
+++ src/main/java/es/jmjg/experiments/domain/tag/exception/TagInUseException.java
@@ -1,6 +1,5 @@
 package es.jmjg.experiments.domain.tag.exception;
 
-//TODO: move all application exceptions to domain
 public class TagInUseException extends RuntimeException {
 
   public TagInUseException(String message) {
